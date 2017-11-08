b75ee7a248deb3b167ef085ffcca5f75e4ac507d
Kamikaze94 <kamikaze1337@live.de>
2017-11-08 21:00:27 +0100

    ExtendedFilters: enhanced item option definition

--- lua/ExtendedFilters.lua
+++ lua/ExtendedFilters.lua
@@ -22,15 +22,29 @@ if requiredScript == "lib/managers/menumanager" then
 	local choice_difficulty_filter_original = MenuCallbackHandler.choice_difficulty_filter
 
 	local server_count = {10, 20, 30, 40, 50, 60, 70}
-	local difficulties = {"menu_any", "menu_difficulty_normal", "menu_difficulty_hard", "menu_difficulty_very_hard", "menu_difficulty_overkill", "menu_difficulty_easy_wish", "menu_difficulty_apocalypse", "menu_difficulty_sm_wish", "menu_difficulty_hard", "menu_difficulty_very_hard", "menu_difficulty_overkill", "menu_difficulty_easy_wish", "menu_difficulty_apocalypse"}
+	local difficulties = {
+		{ name_id = "menu_any", 					value = -1 },
+		{ name_id = "menu_difficulty_normal", 		value = 2 },
+		{ name_id = "menu_difficulty_hard", 		value = 3 },
+		{ name_id = "menu_difficulty_very_hard", 	value = 4 },
+		{ name_id = "menu_difficulty_overkill", 	value = 5 },
+		{ name_id = "menu_difficulty_easy_wish", 	value = 6 },
+		{ name_id = "menu_difficulty_apocalypse", 	value = 7 },
+		{ name_id = "menu_difficulty_sm_wish", 		value = 8 },
+		{ name_id = "menu_difficulty_hard", 		value = 9, 	suffix = "+" },
+		{ name_id = "menu_difficulty_very_hard", 	value = 10, suffix = "+" },
+		{ name_id = "menu_difficulty_overkill", 	value = 11, suffix = "+" },
+		{ name_id = "menu_difficulty_easy_wish", 	value = 12, suffix = "+" },
+		{ name_id = "menu_difficulty_apocalypse", 	value = 13, suffix = "+" },
+	}
 
 	function MenuCrimeNetFiltersInitiator:modify_node(original_node, ...)
-		local res = modify_filter_node_actual(self, original_node, ...)
+		local new_node = modify_filter_node_actual(self, original_node, ...)
 		if server_count ~= nil then
-			local max_lobbies = original_node:item("max_lobbies_filter")
+			local max_lobbies = new_node:item("max_lobbies_filter")
 			if max_lobbies ~= nil then
 				max_lobbies:clear_options()
-				for __, count in ipairs(server_count) do
+				for _, count in ipairs(server_count) do
 					max_lobbies:add_option(CoreMenuItemOption.ItemOption:new({
 						_meta = "option",
 						text_id = tostring(count),
@@ -43,14 +57,14 @@ if requiredScript == "lib/managers/menumanager" then
 		end
 
 		if difficulties ~= nil then
-			local diff_filter = original_node:item("difficulty_filter")
+			local diff_filter = new_node:item("difficulty_filter")
 			if diff_filter ~= nil then
 				diff_filter:clear_options()
-				for k, v in ipairs(difficulties) do
+				for _, v in ipairs(difficulties) do
 					diff_filter:add_option(CoreMenuItemOption.ItemOption:new({
 						_meta = "option",
-						text_id = managers.localization:text(v) .. (k > 8 and " +" or ""),
-						value = k == 1 and -1 or k,
+						text_id = managers.localization:text(v.name_id) .. (v.suffix or ""),
+						value = v.value,
 						localize = false
 					}))
 				end
@@ -62,7 +76,7 @@ if requiredScript == "lib/managers/menumanager" then
 			end
 		end
 
-		return res
+		return new_node
 	end
 
 	function MenuCallbackHandler:choice_difficulty_filter(item, ...)
